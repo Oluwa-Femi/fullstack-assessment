31b079b8e0fc9ceecfd7d698cc3e73ea41d464be
oluwafemi.oluwatola <oluwafemi.oluwatola@sterling.ng>
2022-08-18 10:18:07 +0100

    Implement idle timeout

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Routes from "./config/routes";
+import "./App.css";
 
 function App() {
+  const idleTimeOut = () => {
+    let time;
+    window.onload = reset;
+    document.onkeyup = reset;
+    document.onmousemove = reset;
+
+    function reset() {
+      clearTimeOut(time);
+      time = setTimeOut(() => localStorage.removeItem("_mt_"), 36000000);
+    }
+  };
+
+  window.onload = function () {
+    if (localStorage.getItem("r_me") === false) {
+      idleTimeOut();
+    }
+  };
   return (
     <div className="">
-      < Routes />
+      <Routes />
     </div>
   );
 }
